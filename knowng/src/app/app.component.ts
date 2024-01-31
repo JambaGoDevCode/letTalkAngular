a5ff68ae35c81856ab62daba44f63d931882585f
João Jamba <jambapedrojoao@gmail.com>
2024-02-01 00:30:49 +0100

    generatt component

--- knowng/src/app/app.component.ts
+++ knowng/src/app/app.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { NewComponentComponent } from './component/new-component/new-component.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, NewComponentComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'knowng';
